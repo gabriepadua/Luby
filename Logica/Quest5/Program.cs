4ef76cf76c1087a79beb2cab11175891dfb6d218
Gabriel Pádua <101902928+gabriepadua@users.noreply.github.com>
2025-01-28 18:21:28 -0300

    feat - Adicionando validação do teste indicado no pdf (6.800,00 * 30%)

--- Logica/Quest5/Program.cs
+++ Logica/Quest5/Program.cs
@@ -1,37 +1,37 @@
-﻿using System;
-
 namespace Porcentagem
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             ExecutarCalculadoraDesconto();
         }
+
         static void ExecutarCalculadoraDesconto()
         {
             do
             {
-                Console.Clear();
-                string resultado = CalcularValorComDescontoFormatado();
+                TestarCalcularValorComDescontoFormatado();
+                Console.WriteLine("Insira o valor do produto: ");
+                string valorProduto = Console.ReadLine().Trim();
+
+                Console.WriteLine("Insira o percentual de desconto: ");
+                string desconto = Console.ReadLine().Trim();
+
+                string resultado = CalcularValorComDescontoFormatado(valorProduto, desconto);
                 Console.WriteLine($"Valor com desconto: {resultado}");
                 Console.WriteLine("\nPressione ESC para sair ou qualquer outra tecla para calcular novamente.");
             }
             while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
-        static string CalcularValorComDescontoFormatado()
+
+        static string CalcularValorComDescontoFormatado(string valorProduto, string desconto)
         {
-            Console.WriteLine("Insira o valor do produto: ");
-            string valorProduto = Console.ReadLine().Trim();
+            valorProduto = valorProduto.Replace("R$", "").Replace(",00","").Trim();
+            string valorCalculavel = valorProduto.Replace(".", "").Replace(",", ".");
 
-            // Solicita o valor do desconto
-            Console.WriteLine("Insira o percentual de desconto: ");
-            string desconto = Console.ReadLine().Trim();
-
-            valorProduto = valorProduto.Replace("R$", "").Trim();
-            valorProduto = valorProduto.Replace(".", "").Replace(",", ".");
-
-            if (!double.TryParse(valorProduto, out double valor))
+            if (!double.TryParse(valorCalculavel, out double valor))
             {
                 return "Valor do produto inválido";
             }
@@ -44,10 +44,20 @@ namespace Porcentagem
             }
 
             double valorComDesconto = valor * (1 - (porcentagem / 100));
-
-            string valorComDescontoFormatado = "R$ " + valorComDesconto.ToString("0.00").Replace('.', ',');
+            string valorComDescontoFormatado = "R$ " + valorComDesconto.ToString("N2");
 
             return valorComDescontoFormatado;
+        }
+
+        static void TestarCalcularValorComDescontoFormatado()
+        {
+            string valorProduto = "R$ 6.800,00";
+            string desconto = "30%";
+            string resultadoEsperado = "R$ 4.760,00";
+            string resultadoObtido = CalcularValorComDescontoFormatado(valorProduto, desconto);
+
+            Console.WriteLine($"Teste 1 - Valor do produto: {valorProduto}, Desconto: {desconto}");
+            Console.WriteLine($"Resultado esperado: {resultadoEsperado}, Resultado obtido: {resultadoObtido}");
         }
     }
 }
